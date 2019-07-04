f95b6d199c68b2d7ebe294ba06fde9d4db36b5da
Yutaka Omido <yutaka.omido@gmail.com>
2019-07-04 22:09:08 +0900

    create mock response of create report intent

--- lambda/custom/intents/create-report-intent.js
+++ lambda/custom/intents/create-report-intent.js
@@ -1,14 +1,40 @@
+const moment = require('moment')
+
 module.exports = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'CreateReportIntent';
   },
   handle(handlerInput) {
-    const speechText = 'Hello World!';
+    let date = handlerInput.requestEnvelope.request.intent.slots.date.value;
+    let graphType = handlerInput.requestEnvelope.request.intent.slots.graphType.value;
+
+    const response = this.createResponse({
+      date: date,
+      graphType: graphType
+    })
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .speak(response)
+      // .withSimpleCard('Hello World', speechText)
       .getResponse();
   },
+  createResponse(input) {
+    // バリデーション
+    let errorResponse = null
+    if (!input.date) {
+      errorResponse = 'いつのレポートが欲しいですか？'
+    } else if (!input.graphType) {
+      errorResponse = '円グラフと棒グラフ、どちらでレポートを作成しますか？'
+    }
+
+    // 指定されない場合はエラーメッセージを返却
+    if (errorResponse) {
+      return errorResponse
+    }
+
+    // おうむ返し
+    const dateJP = moment(input.date).format('YYYY年MM月DD日')
+    return `${dateJP}のレポートを${input.graphType}でLineに送信しました。`
+  }
 };
